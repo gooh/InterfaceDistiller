32b307c840e08630ad959798e3844e63f1ae1712
gooh <info@gordon-oheim.de>
2011-11-05 15:15:49 +0100

    added UnitTest for Distillate

---
+++ tests/src/Distillate/DistillateTest.php
@@ -0,0 +1,77 @@
+<?php
+class DistillateTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Distillate
+     */
+    private $distillate;
+
+    /**
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->distillate = new Distillate;
+    }
+
+    /**
+     * @covers Distillate::__construct
+     * @covers Distillate::getInterfaceName
+     */
+    public function testNewDistillateHasDefaultInterfaceName()
+    {
+        $this->assertSame('MyInterface', $this->distillate->getInterfaceName());
+    }
+
+    /**
+     * @covers Distillate::__construct
+     * @covers Distillate::getExtendingInterfaces
+     */
+    public function testNewDistillateDoesNotExtendAnyInterfaces()
+    {
+        $this->assertSame('', $this->distillate->getExtendingInterfaces());
+    }
+
+    /**
+     * @covers Distillate::__construct
+     * @covers Distillate::getInterfaceMethods
+     */
+    public function testNewDistillateDoesNotHaveAnyInterfaceMethods()
+    {
+        $this->assertEquals(array(), $this->distillate->getInterfaceMethods());
+    }
+
+    /**
+     * @covers Distillate::getInterfaceName
+     * @covers Distillate::setInterfaceName
+     */
+    public function testCanGetAndSetInterfaceName()
+    {
+        $this->distillate->setInterfaceName('NewInterface');
+        $this->assertSame('NewInterface', $this->distillate->getInterfaceName());
+    }
+
+    /**
+     * @covers Distillate::getExtendingInterfaces
+     * @covers Distillate::setExtendingInterfaces
+     */
+    public function testCanGetAndSetExtendingInterfaces()
+    {
+        $this->distillate->setExtendingInterfaces('Countable');
+        $this->assertSame('Countable', $this->distillate->getExtendingInterfaces());
+    }
+
+    /**
+     * @covers Distillate::getInterfaceMethods
+     * @covers Distillate::setInterfaceMethods
+     */
+    public function testCanGetAndSetInterfaceMethods()
+    {
+        $method = $this->getMock('ReflectionMethod', array(), array(), '', false);
+        $this->distillate->addMethod($method);
+        $this->assertEquals(
+            array($method),
+            $this->distillate->getInterfaceMethods()
+        );
+    }
+}
